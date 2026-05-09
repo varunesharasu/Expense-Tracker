5ac5a02eb512ebec553c5a396bd93a31f3105471
VaruneshT <tvarunesharasu@gmail.com>
2026-05-09 15:50:24 +0530

    Updated SummaryCards.js

---
+++ frontend/src/components/SummaryCards.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './SummaryCards.css';
+
+function SummaryCards({ transactions }) {
+  const income = transactions
+    .filter((t) => t.type === 'income')
+    .reduce((acc, item) => acc + item.amount, 0);
+
+  const expense = transactions
+    .filter((t) => t.type === 'expense')
+    .reduce((acc, item) => acc + item.amount, 0);
+
+  const balance = income - expense;
+
+  return (
+    <div className='summary-cards'>
+      <div className='card'>
+        <h3>Total Balance</h3>
+        <p>${balance}</p>
+      </div>
+
+      <div className='card'>
+        <h3>Total Income</h3>
+        <p>${income}</p>
+      </div>
+
+      <div className='card'>
+        <h3>Total Expense</h3>
+        <p>${expense}</p>
+      </div>
+    </div>
+  );
+}
+
+export default SummaryCards;
