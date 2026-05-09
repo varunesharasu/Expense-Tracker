f5fee2f7aa914017e377b25dcff2eba673cd3af6
VaruneshT <tvarunesharasu@gmail.com>
2026-05-09 15:55:01 +0530

    Updated TransactionList.js

---
+++ frontend/src/components/TransactionList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './TransactionList.css';
+import API from '../services/api';
+
+function TransactionList({ transactions, fetchTransactions }) {
+  const deleteTransaction = async (id) => {
+    await API.delete(`/transactions/${id}`);
+    fetchTransactions();
+  };
+
+  return (
+    <div className='transaction-list'>
+      <h2>Transactions</h2>
+
+      {transactions.map((item) => (
+        <div className='transaction-item' key={item._id}>
+          <div>
+            <h4>{item.title}</h4>
+            <p>{item.category}</p>
+          </div>
+
+          <div>
+            <span>${item.amount}</span>
+            <button onClick={() => deleteTransaction(item._id)}>
+              Delete
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TransactionList;
